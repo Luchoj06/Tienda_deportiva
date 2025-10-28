5d36b4aa29ff7e8dd5628c7649254855fada3c07
Luchoj06 <soylucho2006@gmail.com>
2025-10-28 09:02:57 -0500

    Update production.php

--- proyecto_empresa/proyecto_empresa/app/Views/errors/cli/production.php
+++ proyecto_empresa/proyecto_empresa/app/Views/errors/cli/production.php
@@ -1,5 +1,7 @@
 <?php
 
-// On the CLI, we still want errors in productions
+// On the CLI, we still want errors in production
 // so just use the exception template.
+
+/* SonarCloud ignore: this include loads a view, not a namespaced class */
 include __DIR__ . '/error_exception.php';
